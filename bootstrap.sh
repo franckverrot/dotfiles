4aa007f31414dbd71c3b9c395d6368b3d2aebabc
Franck Verrot <franck@verrot.fr>
2013-01-23 09:45:05 +0100

    Ensure brew is installed.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,13 @@ cd "$(dirname "$0")"
 # Update the repo
 git pull
 
+# Install required binaries
+
+if ! hash brew 2>&-
+then
+    echo >&2 "I require brew but it's not installed.  Aborting."
+    exit 1
+fi
+
 #FIXME: Warn me I'm gonna lose the changes I've done outside the repo
 rsync --exclude-from .gitignore --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
