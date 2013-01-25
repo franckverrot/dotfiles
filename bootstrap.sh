3df33de04ddd0454821aac4517aca7b52ec186b6
Franck Verrot <franck@verrot.fr>
2013-01-25 09:09:19 +0100

    Add tmux

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,18 +7,14 @@ cd "$(dirname "$0")"
 git pull
 
 # Install required binaries
-
-if ! hash brew 2>&-
-then
-    echo >&2 "I require brew but it's not installed.  Aborting."
-    exit 1
-fi
-
-if ! hash ag 2>&-
-then
-    echo >&2 "I require ag. Please run 'brew install the_silver_searcher'. Aborting."
-    exit 1
-fi
+for cmd in brew ag tmux
+do
+  if ! hash $cmd 2>&-
+  then
+      echo >&2 "I require $cmd but it's not installed. Aborting."
+      exit 1
+  fi
+done
 
 #FIXME: Warn me I'm gonna lose the changes I've done outside the repo
 rsync --exclude-from .gitignore --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
