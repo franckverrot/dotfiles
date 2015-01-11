f68ced7235d44f9cb19ddd98d31ab026c11b63ac
Franck Verrot <franck@verrot.fr>
2015-01-11 13:34:58 +0100

    Add elixir's files

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,4 +17,4 @@ do
 done
 
 #FIXME: Warn me I'm gonna lose the changes I've done outside the repo
-rsync --exclude-from .gitignore --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+rsync --exclude-from .gitignore --exclude ".git/" --exclude ".DS_Store" --exclude ".gitmodules" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
